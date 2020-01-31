25dfdffb95e712de534240ef2140c677a9ba5d90
fly1057@yeah.net <fly1057@yeah.net>
2020-01-31 13:23:45 +0800

    master

--- ABB方式_考虑采样时间、晶闸管时间常数.py
+++ ABB方式_考虑采样时间、晶闸管时间常数.py
@@ -17,8 +17,8 @@ import  os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 步骤一（替换sans-serif字体）
-plt.rcParams['axes.unicode_minus'] = False   # 步骤二（解决坐标轴负数的负号显示问题）
+#plt.rcParams['font.sans-serif'] = ['SimHei']  # 步骤一（替换sans-serif字体）
+#plt.rcParams['axes.unicode_minus'] = False   # 步骤二（解决坐标轴负数的负号显示问题）
 
 
 class Model():
